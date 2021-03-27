fdf00794d12110f70cc62e4417c780a6e01150b3
이주상 <wntkd0530@naver.com>
2021-03-27 13:24:46 +0900

    [Feat] add gu boundarys by Polygon API
    - 오픈 API 속 폴리곤 정보를 활용하여, 군 별 폴리곤 표시
    - 그 외, dong 좌표값 파싱한 json 파일을 활용하여 동별 원형 마커 표시 완료 -> (현재 미활용)

--- frontend/src/views/MapSearch.vue
+++ frontend/src/views/MapSearch.vue
@@ -17,11 +17,12 @@
          <side-menu @open-input-form="onClickInputBt"></side-menu>
       </div>
 
-      <vue-daum-map id="map" :appKey="appKey" :center.sync="center" :level.sync="level" :mapTypeId="mapTypeId" :libraries="libraries" @load="onLoad"> </vue-daum-map>
+      <vue-daum-map id="map" :appKey="appKey" :center.sync="center" :level.sync="level" :mapTypeId="mapTypeId" :libraries="libraries" @load="onLoad" @tilesloaded="onMapEvent('titlesloaded', $event)">
+      </vue-daum-map>
    </div>
 </template>
 
-<script type="text/javascript" src="http://dapi.kakao.com/v2/maps/sdk.js?appkey=5cc03bac0d3510a482068b50dd6e3612"></script>
+<script type="text/javascript" src="http://dapi.kakao.com/v2/maps/sdk.js?appkey=5cc03bac0d3510a482068b50dd6e3612&libraries=services,clusterer"></script>
 <script>
 import VueDaumMap from 'vue-daum-map';
 import MapDetail from '@/components/MapDetail/MapDetail.vue';
@@ -29,6 +30,9 @@ import BookmarkList from '@/components/Bookmark/BookmarkList.vue';
 import OptionInput from '@/components/OptionInput.vue';
 import SideMenu from '@/components/SideMenu.vue';
 import axios from 'axios';
+
+// 동코드 JSON 파일 import
+import dongCoords from '@/assets/data/dong_coords.json';
 
 export default {
    components: { VueDaumMap, MapDetail, OptionInput, SideMenu, BookmarkList },
@@ -48,6 +52,8 @@ export default {
       customOverlays: [],
       circles: [],
       polygons: [],
+      dongMarkers: [],
+      boundarys: [],
 
       // 테스트를 위한 샘플 코드
       sample: [
@@ -70,6 +76,9 @@ export default {
             y: '449448',
          },
       ],
+
+      // 동별로 마커를 찍기 위한 json파일 import
+      dongCoords: dongCoords,
    }),
    created() {
       this.initCenter();
@@ -90,16 +99,125 @@ export default {
       },
       // 지도가 로드 완료되면 load 이벤트 발생
       onLoad(map) {
-         // console.log(map);
+         console.log('onLoad');
          this.mapObject = map;
       },
       initCenter() {
+         console.log('initCenter');
          // 중심위치 세팅
-         this.center.lat = 37.567538;
-         this.center.lng = 127.004528;
+         this.center.lat = 37.567538; // 위도
+         this.center.lng = 127.004528; // 경로
+         this.level = 8;
+
+         // 축약, 카피라이트 우측 표시
+         // this.mapObject.setCopyrightPosition(kakao.maps.CopyrightPosition.BOTTOMRIGHT, true);
       },
 
-      //좌표 변환
+      // 동별로 마커 표시
+      setDongMarker() {
+         var clusterer = new kakao.maps.MarkerClusterer({
+            map: this.mapObject, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+            minLevel: 10, // 클러스터 할 최소 지도 레벨
+            calculator: [10, 30, 50],
+            texts: getTexts, // texts는 ['삐약', '꼬꼬', '꼬끼오', '치멘'] 이렇게 배열로도 설정할 수 있다
+            styles: [
+               {
+                  // calculator 각 사이 값 마다 적용될 스타일을 지정한다
+                  width: '30px',
+                  height: '30px',
+                  background: 'rgba(51, 204, 255, .8)',
+                  borderRadius: '15px',
+                  color: '#000',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  lineHeight: '31px',
+               },
+               {
+                  width: '40px',
+                  height: '40px',
+                  background: 'rgba(255, 153, 0, .8)',
+                  borderRadius: '20px',
+                  color: '#000',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  lineHeight: '41px',
+               },
+               {
+                  width: '50px',
+                  height: '50px',
+                  background: 'rgba(255, 51, 204, .8)',
+                  borderRadius: '25px',
+                  color: '#000',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  lineHeight: '51px',
+               },
+               {
+                  width: '60px',
+                  height: '60px',
+                  background: 'rgba(255, 80, 80, .8)',
+                  borderRadius: '30px',
+                  color: '#000',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                  lineHeight: '61px',
+               },
+            ],
+         });
+
+         // 클러스터 내부에 삽입할 문자열 생성 함수입니다
+         function getTexts(count) {
+            // 한 클러스터 객체가 포함하는 마커의 개수에 따라 다른 텍스트 값을 표시합니다
+            if (count < 10) {
+               return '삐약';
+            } else if (count < 30) {
+               return '꼬꼬';
+            } else if (count < 50) {
+               return '꼬끼오';
+            } else {
+               return '치멘';
+            }
+         }
+
+         // console.log(clusterer);
+
+         for (var idx in this.dongCoords) {
+            var position = new kakao.maps.LatLng(this.dongCoords[idx].lat, this.dongCoords[idx].lng);
+            var content = `<div class="cell">
+                              ${this.dongCoords[idx].dong}
+                           </div>`;
+
+            // 커스텀 오버레이를 생성합니다
+            var dongOverlay = new kakao.maps.CustomOverlay({
+               map: this.mapObject,
+               position: position,
+               content: content,
+            });
+
+            // dongOverlay.setMap(this.mapObject);
+            this.dongMarkers.push(dongOverlay);
+         }
+
+         clusterer.addMarkers(this.dongMarkers);
+
+         // 마커 클러스터러를 생성합니다
+      },
+
+      onMapEvent(event) {
+         console.log('onMapEvent : ' + event);
+
+         var bounds = this.mapObject.getBounds();
+         // ha: 126.99393601590647
+         // oa: 127.1044468844557
+         // pa: 37.59540448930552
+         // qa: 37.52942551830513
+
+         this.removeBoundarys();
+         this.getPolygon('gu', bounds);
+      },
+
+      //좌표 변환 (상권 데이터 x,y -> 카카오맵)
       coordsChange(district) {
          // 상권 데이터 = EPSG 5181 / 카카오맵 = WGS84 사용
          // 변환 과정 : EPSG -> wcong(kakao.map.Coords) * 2.5 -> toLatLng()
@@ -146,7 +264,9 @@ export default {
                yAnchor: 0.8,
             });
 
-            this.getPolygonAPI(position);
+            // console.log(customOverlay);
+
+            this.getPolygonDistrict(position);
 
             // 마커를 생성합니다
             marker = new kakao.maps.Marker({
@@ -186,14 +306,81 @@ export default {
          this.polygons = [];
       },
 
-      //오픈 API를 통해 폴리곤 정보를 불러옴
-      getPolygonAPI(position) {
+      // 구, 동 등의 경계 정보를 초기화함
+      removeBoundarys() {
+         for (const key in this.boundarys) {
+            this.boundarys[key].setMap(null);
+         }
+         this.boundarys = [];
+      },
+
+      // 구, 동을 구분하는 폴리곤 정보를 요청함
+      getPolygon(type, bounds) {
          var key = '13C339D4-B453-3C5E-A6A1-CCA6792A2D6B'; // 공간정보 오픈플랫폼
          var domain = 'http://localhost:8080';
+         var crs = 'EPSG:4326'; // 반환되는 좌표(WGS84)
+         var geo = `BOX(${bounds.ha},${bounds.qa},${bounds.oa},${bounds.pa})`; //minx, miny, maxx, maxy로 검색 범위 설정
+         var filter = 'sig_cd:<=:11740'; // 11740이 서울시 마지막 행정동코드(강동구)
+         var data;
+
+         // 구 정보를 반환해서 폴리곤에 표시
+         if (type == 'gu') {
+            data = 'LT_C_ADSIGG_INFO'; // 시군구 조회
+         } else {
+            data = 'LT_C_ADEMD_INFO'; // 읍면동 조회
+         }
+
+         axios
+            .get(`http://api.vworld.kr/req/data?request=GetFeature&data=${data}&key=${key}&format=json&domain=${domain}&crs=${crs}&geomFilter=${geo}&attrFilter=${filter}&size=100`)
+            .then((response) => {
+               var local = response.data.response.result.featureCollection.features;
+
+               for (const key in local) {
+                  var polygonArr = local[key].geometry.coordinates[0][0];
+                  var polygonPath = [];
+                  var korName = local[key].properties.sig_kor_nm;
+
+                  // 해당 데이터를 polygonPath에서 원하는 방식으로 맵핑
+                  for (let i = 0; i < polygonArr.length; i++) {
+                     polygonPath.push(new kakao.maps.LatLng(polygonArr[i][1], polygonArr[i][0]));
+                  }
+
+                  this.setPolygonBoundary(polygonPath, korName);
+               }
+            })
+            .catch((err) => {
+               console.log('ERROR : ' + err);
+            });
+      },
+
+      // 지도에 시, 동 경계 및 이름을 표시
+      setPolygonBoundary(polygonPath, korName) {
+         var polygon = new kakao.maps.Polygon({
+            path: polygonPath, // 그려질 다각형의 좌표 배열입니다
+            strokeWeight: 3, // 선의 두께입니다
+            strokeColor: '#212121', // 선의 색깔입니다
+            strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'longdash', // 선의 스타일입니다
+            fillColor: '#141414', // 채우기 색깔입니다
+            fillOpacity: 0.1, // 채우기 불투명도 입니다
+         });
+
+         // 지도에 다각형을 표시합니다
+         polygon.setMap(this.mapObject);
+         this.boundarys.push(polygon);
+      },
+
+      //오픈 API를 통해 폴리곤 정보를 불러옴
+      getPolygonDistrict(position) {
+         var key = '13C339D4-B453-3C5E-A6A1-CCA6792A2D6B'; // 공간정보 오픈플랫폼
+         var domain = 'http://localhost:8080';
+         var crs = 'EPSG:4326'; // 반환되는 좌표(WGS84)
+         var geo = `POINT(${position.La} ${position.Ma})`;
+         var data = 'LT_C_DGMAINBIZ'; // 상권 폴리곤 출력
          // console.log(position.La, position.Ma);
 
          axios
-            .get(`http://api.vworld.kr/req/data?request=GetFeature&data=LT_C_DGMAINBIZ&key=${key}&format=json&domain=${domain}&crs=EPSG:4326&geomFilter=POINT(${position.La} ${position.Ma})`)
+            .get(`http://api.vworld.kr/req/data?request=GetFeature&data=${data}&key=${key}&format=json&domain=${domain}&crs=${crs}&geomFilter=${geo}`)
             .then((response) => {
                // console.log(response.data.response);
                if (response.data.response.status == 'OK') {
@@ -257,7 +444,7 @@ export default {
    },
 };
 </script>
-<style scoped lang="scss">
+<style scoed lang="scss">
 .container {
    background-color: white;
    width: 100%;
