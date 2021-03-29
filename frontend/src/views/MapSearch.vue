32e114449b8908117a847b1ed03ec98038822f36
이주상 <wntkd0530@naver.com>
2021-03-30 07:57:09 +0900

    [Feat] add Marker Clusterer
    - 구정보 코드 json 추가
    - 동정보 코드 json 파일 변경
    - 마커 클러스터링 구현

--- frontend/src/views/MapSearch.vue
+++ frontend/src/views/MapSearch.vue
@@ -17,7 +17,17 @@
          <side-menu @open-input-form="onClickInputBt"></side-menu>
       </div>
 
-      <vue-daum-map id="map" :appKey="appKey" :center.sync="center" :level.sync="level" :mapTypeId="mapTypeId" :libraries="libraries" @load="onLoad" @tilesloaded="onMapEvent('titlesloaded', $event)">
+      <vue-daum-map
+         id="map"
+         :appKey="appKey"
+         :center.sync="center"
+         :level.sync="level"
+         :mapTypeId="mapTypeId"
+         :libraries="libraries"
+         @load="onLoad"
+         @tilesloaded="onMapEvent('titlesloaded', $event)"
+         @zoom_changed="onMapEvent('zoom_changed', $event)"
+      >
       </vue-daum-map>
    </div>
 </template>
@@ -32,6 +42,7 @@ import SideMenu from '@/components/SideMenu.vue';
 import axios from 'axios';
 
 // 동코드 JSON 파일 import
+import guCoords from '@/assets/data/gu_coords.json';
 import dongCoords from '@/assets/data/dong_coords.json';
 
 export default {
@@ -52,8 +63,11 @@ export default {
       customOverlays: [],
       circles: [],
       polygons: [],
-      dongMarkers: [],
-      boundarys: [],
+
+      // 구별 경계 등에 대한 정보
+      gu_Boundarys: [],
+      gu_Overlays: [],
+      dong_Overlays: [],
 
       // 테스트를 위한 샘플 코드
       sample: [
@@ -78,11 +92,13 @@ export default {
       ],
 
       // 동별로 마커를 찍기 위한 json파일 import
+      guCoords: guCoords,
       dongCoords: dongCoords,
    }),
    created() {
       this.initCenter();
    },
+
    methods: {
       // sidemenu의 옵션입력 버튼 눌렀을 때
       onClickInputBt: function() {
@@ -101,131 +117,202 @@ export default {
       onLoad(map) {
          console.log('onLoad');
          this.mapObject = map;
+
+         // 시작 되자마자 폴리곤 셋팅
+         // var bounds = this.mapObject.getBounds();
+         var bounds = {
+            ha: 126.72845050681946,
+            qa: 37.437210868999315,
+            oa: 127.17018320519693,
+            pa: 37.7014997773775,
+         };
+         this.getPolygon('gu', bounds);
+
+         this.setDongMarker('make');
       },
+
       initCenter() {
          console.log('initCenter');
          // 중심위치 세팅
-         this.center.lat = 37.567538; // 위도
-         this.center.lng = 127.004528; // 경로
-         this.level = 8;
-
-         // 축약, 카피라이트 우측 표시
-         // this.mapObject.setCopyrightPosition(kakao.maps.CopyrightPosition.BOTTOMRIGHT, true);
-      },
-
-      // 동별로 마커 표시
-      setDongMarker() {
-         var clusterer = new kakao.maps.MarkerClusterer({
-            map: this.mapObject, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-            minLevel: 10, // 클러스터 할 최소 지도 레벨
-            calculator: [10, 30, 50],
-            texts: getTexts, // texts는 ['삐약', '꼬꼬', '꼬끼오', '치멘'] 이렇게 배열로도 설정할 수 있다
-            styles: [
-               {
-                  // calculator 각 사이 값 마다 적용될 스타일을 지정한다
-                  width: '30px',
-                  height: '30px',
-                  background: 'rgba(51, 204, 255, .8)',
-                  borderRadius: '15px',
-                  color: '#000',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  lineHeight: '31px',
-               },
-               {
-                  width: '40px',
-                  height: '40px',
-                  background: 'rgba(255, 153, 0, .8)',
-                  borderRadius: '20px',
-                  color: '#000',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  lineHeight: '41px',
-               },
-               {
-                  width: '50px',
-                  height: '50px',
-                  background: 'rgba(255, 51, 204, .8)',
-                  borderRadius: '25px',
-                  color: '#000',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  lineHeight: '51px',
-               },
-               {
-                  width: '60px',
-                  height: '60px',
-                  background: 'rgba(255, 80, 80, .8)',
-                  borderRadius: '30px',
-                  color: '#000',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  lineHeight: '61px',
-               },
-            ],
-         });
-
-         // 클러스터 내부에 삽입할 문자열 생성 함수입니다
-         function getTexts(count) {
-            // 한 클러스터 객체가 포함하는 마커의 개수에 따라 다른 텍스트 값을 표시합니다
-            if (count < 10) {
-               return '삐약';
-            } else if (count < 30) {
-               return '꼬꼬';
-            } else if (count < 50) {
-               return '꼬끼오';
-            } else {
-               return '치멘';
-            }
-         }
-
-         // console.log(clusterer);
-
-         for (var idx in this.dongCoords) {
-            var position = new kakao.maps.LatLng(this.dongCoords[idx].lat, this.dongCoords[idx].lng);
-            var content = `<div class="cell">
-                              ${this.dongCoords[idx].dong}
-                           </div>`;
-
-            // 커스텀 오버레이를 생성합니다
-            var dongOverlay = new kakao.maps.CustomOverlay({
-               map: this.mapObject,
-               position: position,
-               content: content,
-            });
-
-            // dongOverlay.setMap(this.mapObject);
-            this.dongMarkers.push(dongOverlay);
-         }
-
-         clusterer.addMarkers(this.dongMarkers);
-
-         // 마커 클러스터러를 생성합니다
+         this.center.lat = 37.5642135; // 위도
+         this.center.lng = 127.0016985; // 경로
+         this.level = 7;
       },
 
       onMapEvent(event) {
-         console.log('onMapEvent : ' + event);
-
+         // console.log('onMapEvent : ' + event + ' / level : ' + this.level);
          var bounds = this.mapObject.getBounds();
-         // ha: 126.99393601590647
-         // oa: 127.1044468844557
-         // pa: 37.59540448930552
-         // qa: 37.52942551830513
 
-         this.removeBoundarys();
-         this.getPolygon('gu', bounds);
+         if (this.level >= 7) {
+            this.setGuMarker('make');
+            // this.setDongMarker('del');
+         } else if (this.level <= 6) {
+            this.setGuMarker('del');
+            // this.getPolygon('dong', bounds);
+         }
       },
 
-      //좌표 변환 (상권 데이터 x,y -> 카카오맵)
-      coordsChange(district) {
-         // 상권 데이터 = EPSG 5181 / 카카오맵 = WGS84 사용
-         // 변환 과정 : EPSG -> wcong(kakao.map.Coords) * 2.5 -> toLatLng()
+      // =========================================
+      //             구/동 별 구획을 표시
+      //   추천 받지 않는 상황에서 지도에 경계 구역을 표시
+      // =========================================
 
-         var coords = new kakao.maps.Coords(district.x * 2.5, district.y * 2.5);
-         // console.log('WGS84 변환', coords.toLatLng());
-         return coords.toLatLng();
+      // 구별로 마커 표시
+      setGuMarker(type) {
+         if (type == 'make') {
+            for (var idx in this.guCoords) {
+               var position = new kakao.maps.LatLng(this.guCoords[idx].lat, this.guCoords[idx].lng);
+
+               var content = `<div class="cell">
+                                 ${this.guCoords[idx].sig_kor_nm}
+                              </div>`;
+
+               // 커스텀 오버레이를 생성합니다
+               var guOverlay = new kakao.maps.CustomOverlay({
+                  map: this.mapObject,
+                  position: position,
+                  content: content,
+               });
+
+               // dongOverlay.setMap(this.mapObject);
+               this.gu_Overlays.push(guOverlay);
+            }
+         } else if (type == 'del' && this.gu_Overlays.length != 0) {
+            for (const key in this.gu_Overlays) {
+               this.gu_Overlays[key].setMap(null);
+            }
+            this.gu_Overlays = [];
+         }
       },
+
+      // 동별로 마커 표시
+      setDongMarker(type) {
+         if (type == 'make') {
+            var clusterer = new kakao.maps.MarkerClusterer({
+               map: this.mapObject, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+               averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+               minLevel: 3, // 클러스터 할 최소 지도 레벨
+               disableClickZoom: true, // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
+            });
+
+            this.dongCoords.forEach((item) => {
+               var position = new kakao.maps.LatLng(item.lat, item.lng);
+
+               var content = `<div class="cell">
+                              ${item.emd_nm}
+                           </div>`;
+
+               // 커스텀 오버레이를 생성합니다
+               var dongOverlay = new kakao.maps.CustomOverlay({
+                  map: this.mapObject,
+                  position: position,
+                  content: content,
+               });
+
+               dongOverlay.setMap(this.mapObject);
+               this.dong_Overlays.push(dongOverlay);
+            });
+
+            clusterer.addMarkers(this.dong_Overlays);
+
+            kakao.maps.event.addListener(clusterer, 'clusterclick', function(cluster) {
+               // 현재 지도 레벨에서 1레벨 확대한 레벨
+               var level = cluster._map.getLevel() - 1;
+
+               // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+               cluster._map.setLevel(level, { anchor: cluster.getCenter() });
+            });
+         } else if (type == 'del' && this.dong_Overlays.length != 0) {
+            for (const key in this.dong_Overlays) {
+               this.dong_Overlays[key].setMap(null);
+            }
+            this.dong_Overlays = [];
+         }
+      },
+
+      // 구, 동 등의 경계 정보를 초기화함
+      removeBoundarys() {
+         for (const key in this.gu_Boundarys) {
+            this.gu_Boundarys[key].setMap(null);
+         }
+         this.gu_Boundarys = [];
+      },
+
+      // 구, 동을 구분하는 폴리곤 정보를 요청함
+      getPolygon(type, bounds) {
+         var key = '13C339D4-B453-3C5E-A6A1-CCA6792A2D6B'; // 공간정보 오픈플랫폼
+         var domain = 'http://localhost:8080';
+         var crs = 'EPSG:4326'; // 반환되는 좌표(WGS84)
+         var geo = `BOX(${bounds.ha},${bounds.qa},${bounds.oa},${bounds.pa})`; //minx, miny, maxx, maxy로 검색 범위 설정
+         var filter = 'full_nm:like:서울'; // 11740이 서울시 마지막 행정동코드(강동구)
+         var data;
+
+         // 구 정보를 반환해서 폴리곤에 표시
+         if (type == 'gu') {
+            data = 'LT_C_ADSIGG_INFO'; // 시군구 조회
+         } else if (type == 'dong') {
+            data = 'LT_C_ADEMD_INFO'; // 읍면동 조회
+         }
+
+         axios
+            .get(`http://api.vworld.kr/req/data?request=GetFeature&data=${data}&key=${key}&format=json&domain=${domain}&crs=${crs}&geomFilter=${geo}&attrFilter=${filter}&size=100`)
+            .then((response) => {
+               var local = response.data.response.result.featureCollection.features;
+
+               for (const key in local) {
+                  var polygonArr = local[key].geometry.coordinates[0][0];
+                  var polygonPath = [];
+                  var korName = local[key].properties.sig_kor_nm;
+
+                  // 해당 데이터를 polygonPath에서 원하는 방식으로 맵핑
+                  for (let i = 0; i < polygonArr.length; i++) {
+                     polygonPath.push(new kakao.maps.LatLng(polygonArr[i][1], polygonArr[i][0]));
+                  }
+
+                  this.setPolygonBoundary(polygonPath, korName);
+               }
+            })
+            .catch((err) => {
+               console.log('ERROR : ' + err);
+            });
+      },
+
+      // 지도에 시, 동 경계 및 이름을 표시
+      setPolygonBoundary(polygonPath, korName) {
+         var polygon = new kakao.maps.Polygon({
+            path: polygonPath, // 그려질 다각형의 좌표 배열입니다
+            strokeWeight: 2, // 선의 두께입니다
+            strokeColor: '#212121', // 선의 색깔입니다
+            strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'longdash', // 선의 스타일입니다
+            fillColor: '#fff', // 채우기 색깔입니다
+            fillOpacity: 0, // 채우기 불투명도 입니다
+         });
+
+         // 지도에 구 정보 다각형을 표시합니다
+         polygon.setMap(this.mapObject);
+         this.gu_Boundarys.push(polygon);
+
+         // var gu_position = new kakao.maps.LatLng(this.dongCoords[idx].lat, this.dongCoords[idx].lng);
+         // var gu_content = `<div class="cell">
+         //                      ${korName}
+         //                   </div>`;
+
+         // // 커스텀 오버레이를 생성합니다
+         // var guOverlay = new kakao.maps.CustomOverlay({
+         //    map: this.mapObject,
+         //    position: gu_position,
+         //    content: gu_content,
+         // });
+
+         // guOverlay.setMap(this.mapObject);
+         // this.gu_Overlays.push(guOverlay);
+      },
+
+      // =========================================
+      //               상권 관련 함수들
+      //  추천 받은 상권들에 대해, 커스텀 마커 + 폴리곤 반환
+      // =========================================
 
       //추천 상권 마커 표시
       setRecommendMarker() {
@@ -284,90 +371,6 @@ export default {
             customOverlay.setMap(this.mapObject);
             this.customOverlays.push(customOverlay);
          }
-      },
-
-      //레이어(마커, 커스텀오버레이, 폴리곤 등) 삭제
-      removeLayers() {
-         for (var i = 0; i < this.markers.length; i++) {
-            this.markers[i].setMap(null);
-            this.customOverlays[i].setMap(null);
-         }
-
-         // 원, 폴리곤 초기화. 갯수가 상이하므로, 마커 갯수(총 표시) 만큼 돌리면서, i값이 사이즈별로 판별
-         for (var i = 0; i < this.markers.length; i++) {
-            if (i < this.circles.length) this.circles[i].setMap(null);
-            if (i < this.polygons.length) this.polygons[i].setMap(null);
-         }
-
-         // 배열 초기화
-         this.markers = [];
-         this.customOverlays = [];
-         this.circles = [];
-         this.polygons = [];
-      },
-
-      // 구, 동 등의 경계 정보를 초기화함
-      removeBoundarys() {
-         for (const key in this.boundarys) {
-            this.boundarys[key].setMap(null);
-         }
-         this.boundarys = [];
-      },
-
-      // 구, 동을 구분하는 폴리곤 정보를 요청함
-      getPolygon(type, bounds) {
-         var key = '13C339D4-B453-3C5E-A6A1-CCA6792A2D6B'; // 공간정보 오픈플랫폼
-         var domain = 'http://localhost:8080';
-         var crs = 'EPSG:4326'; // 반환되는 좌표(WGS84)
-         var geo = `BOX(${bounds.ha},${bounds.qa},${bounds.oa},${bounds.pa})`; //minx, miny, maxx, maxy로 검색 범위 설정
-         var filter = 'sig_cd:<=:11740'; // 11740이 서울시 마지막 행정동코드(강동구)
-         var data;
-
-         // 구 정보를 반환해서 폴리곤에 표시
-         if (type == 'gu') {
-            data = 'LT_C_ADSIGG_INFO'; // 시군구 조회
-         } else {
-            data = 'LT_C_ADEMD_INFO'; // 읍면동 조회
-         }
-
-         axios
-            .get(`http://api.vworld.kr/req/data?request=GetFeature&data=${data}&key=${key}&format=json&domain=${domain}&crs=${crs}&geomFilter=${geo}&attrFilter=${filter}&size=100`)
-            .then((response) => {
-               var local = response.data.response.result.featureCollection.features;
-
-               for (const key in local) {
-                  var polygonArr = local[key].geometry.coordinates[0][0];
-                  var polygonPath = [];
-                  var korName = local[key].properties.sig_kor_nm;
-
-                  // 해당 데이터를 polygonPath에서 원하는 방식으로 맵핑
-                  for (let i = 0; i < polygonArr.length; i++) {
-                     polygonPath.push(new kakao.maps.LatLng(polygonArr[i][1], polygonArr[i][0]));
-                  }
-
-                  this.setPolygonBoundary(polygonPath, korName);
-               }
-            })
-            .catch((err) => {
-               console.log('ERROR : ' + err);
-            });
-      },
-
-      // 지도에 시, 동 경계 및 이름을 표시
-      setPolygonBoundary(polygonPath, korName) {
-         var polygon = new kakao.maps.Polygon({
-            path: polygonPath, // 그려질 다각형의 좌표 배열입니다
-            strokeWeight: 3, // 선의 두께입니다
-            strokeColor: '#212121', // 선의 색깔입니다
-            strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: 'longdash', // 선의 스타일입니다
-            fillColor: '#141414', // 채우기 색깔입니다
-            fillOpacity: 0.1, // 채우기 불투명도 입니다
-         });
-
-         // 지도에 다각형을 표시합니다
-         polygon.setMap(this.mapObject);
-         this.boundarys.push(polygon);
       },
 
       //오픈 API를 통해 폴리곤 정보를 불러옴
@@ -440,6 +443,57 @@ export default {
          // 지도에 다각형을 표시합니다
          polygon.setMap(this.mapObject);
          this.polygons.push(polygon);
+      },
+
+      //레이어(마커, 커스텀오버레이, 폴리곤 등) 삭제
+      removeLayers() {
+         for (var i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(null);
+            this.customOverlays[i].setMap(null);
+         }
+
+         // 원, 폴리곤 초기화. 갯수가 상이하므로, 마커 갯수(총 표시) 만큼 돌리면서, i값이 사이즈별로 판별
+         for (var i = 0; i < this.markers.length; i++) {
+            if (i < this.circles.length) this.circles[i].setMap(null);
+            if (i < this.polygons.length) this.polygons[i].setMap(null);
+         }
+
+         // 배열 초기화
+         this.markers = [];
+         this.customOverlays = [];
+         this.circles = [];
+         this.polygons = [];
+      },
+
+      // =========================================
+      //        메소드 호출에 따른 부가 기능 함수들
+      // =========================================
+
+      // 좌표 변환 (상권 데이터 x,y -> 카카오맵)
+      coordsChange(district) {
+         // 상권 데이터 = EPSG 5181 / 카카오맵 = WGS84 사용
+         // 변환 과정 : EPSG -> wcong(kakao.map.Coords) * 2.5 -> toLatLng()
+
+         var coords = new kakao.maps.Coords(district.x * 2.5, district.y * 2.5);
+         // console.log('WGS84 변환', coords.toLatLng());
+         return coords.toLatLng();
+      },
+
+      // 폴리곤을 통해 중심 좌표 구하기(centroid 알고리즘)
+      centroid(polygonPath) {
+         var i, j, len, p1, p2, f, area, x, y;
+
+         area = x = y = 0;
+         for (i = 0, len = point.length, j = len - 1; i < len; j = i++) {
+            p1 = point[i];
+            p2 = point[j];
+            f = p1[1] * p2[0] - p2[1] * p1[0];
+            x += (p1[0] + p2[0]) * f;
+            y += (p1[1] + p2[1]) * f;
+            area += f * 3;
+         }
+
+         return new kakao.maps.LatLng(x / area, y / area);
       },
    },
 };
