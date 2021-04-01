a231c1a4297d99829a152ab6b883a07e3770e396
김수연 <xsw7563@naver.com>
2021-04-01 14:16:13 +0900

    [Feat] Add pie-chart and line graph

---
+++ frontend/src/components/MapDetail/PieChart.vue
@@ -0,0 +1,42 @@
+<script>
+import {Pie} from 'vue-chartjs'
+
+export default {
+    extends: Pie,
+    data: function() {
+        return {
+            datacollection: {
+                labels: [],
+                datasets: [
+                    {
+                        backgroundColor: ['rgba(220, 25, 25, 0.8)', 'rgba(54, 162, 235, 0.8)'],
+                        pointBackgroundColor: 'white',
+                        pointBorderColor: '#249EBF',
+                        data: []
+                    }
+                ]
+            },
+            options: {
+                maintainAspectRatio: true,
+            }
+        }
+    },
+    props: {
+        dataset: Object
+    },
+    methods: {
+        getValue: function() {
+            this.datacollection.labels = this.dataset.category
+            this.datacollection.datasets[0].data = this.dataset.value
+        }
+    },
+    mounted () {
+        this.getValue()
+        this.renderChart(this.datacollection, this.options)
+    }
+}
+</script>
+
+<style>
+
+</style>
